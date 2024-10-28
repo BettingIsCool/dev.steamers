de323f14a44ee3aab851f8ef758de25dd9734ec4
BettingIsCool <contact@bettingiscool.com>
2024-10-28 11:31:20 +0100

    Add Telegram user id handling and new .idea gitignore entries
    Implemented a function to retrieve Telegram user IDs from the database and added it to the session state in `streamlit_app.py`. Updated the sidebar to prompt users to connect their Telegram if not already connected. Also, included new .idea configuration files in the .gitignore.

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ if st.session_state.session_id == toolkit.get_active_session(st.session_state.us
 
     if st.session_state.telegram_user_id is None:
         #st.sidebar.link_button(label='Connect Telegram', url='https://t.me/psp_ultra_bot', help='Hit this button to receive telegram alerts.', type='primary')
-        st.button('Open link', on_click=toolkit.open_page, args=('https://t.me/psp_ultra_bot',))
+        st.button('Open link', on_click=toolkit.open_page(url='https://t.me/psp_ultra_bot'))
         st.session_state.telegram_user_id = db.get_telegram_user_id(username=username)[0]
 
     st.sidebar.subheader(f"Default settings")
